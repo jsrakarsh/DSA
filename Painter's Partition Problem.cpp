55609a1f80bc075a0ff3e1342cd286bfdff66791
jsrakarsh <104302027+jsrakarsh@users.noreply.github.com>
2023-10-03 01:14:03 +0530

    Add files via upload

---
+++ Painter's Partition Problem.cpp
@@ -0,0 +1,50 @@
+int no_of_students(vector<int>arr,int value)
+{   
+    // value means the no.of pages for which we have to check;
+    int students=1; 
+    int current=0;  // this variable to show the current page holded by student 1
+    for(int i=0;i<arr.size();i++)
+    {
+        if(arr[i]+current <= value)
+        {
+            current+= arr[i];
+        }
+        else{
+            students++ ; //when limit exceeds to hold page..shift to next student
+                        // and update the current to 0;
+            current = arr[i];
+        }
+
+    }
+    return students;
+}
+int findPages(vector<int>& arr, int n, int m) {
+    //if no. of students exceeds than no. of books in array
+    if(m>n)
+    {
+        return -1;
+    }
+    int ans;
+    int low = *max_element(arr.begin(),arr.end());
+    int high = accumulate(arr.begin(),arr.end(), 0 );
+    while(low<=high)
+    {
+        int mid=(low+high)/2;
+        int students= no_of_students(arr,mid);
+        if(students > m)
+        {   
+            low = mid+1;
+        }
+        else        // this question is same to same as book allocation
+                        //read the notes section of take u forward..for this question
+        {
+            ans = mid;
+            high =mid -1;
+        }
+    }
+    return ans;
+}
+int findLargestMinDistance(vector<int> &boards, int k)
+{
+    return findPages(boards,boards.size(),k);
+}
